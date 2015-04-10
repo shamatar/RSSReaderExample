7984f3b7b59755e4906d6165c722657697729515
Alexander Vlasov <alexvlasov@MacBook-Pro.local>
2015-04-10 11:45:05 -0400

    Initial Commit

---
+++ RSSReaderExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReaderExample
+//
+//  Created by Alexander Vlasov on 10.04.15.
+//  Copyright (c) 2015 Alexander Vlasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
